93fcf3019079352c3e5db8dd3d8b25f109d0bd72
Sayantan Datta <std2048@gmail.com>
2013-08-11 19:11:08 +0530

    fixed mask-modes indentations.

--- src/opencl_shared_mask.h
+++ src/opencl_shared_mask.h
@@ -14,33 +14,33 @@
 
   /* Range of characters for a placeholder in the mask */
 struct mask_range {
-  /* Characters in the range */
+	/* Characters in the range */
 	unsigned char chars[0x100];
 
-  /* Set to zero when the characters in the range are not consecutive,
-   * otherwise start is set to the minumum value in range. Minimum
-   * value cannot be a null character which has a value zero.*/
-  	unsigned char start;
+	/* Set to zero when the characters in the range are not consecutive,
+	 * otherwise start is set to the minumum value in range. Minimum
+	 * value cannot be a null character which has a value zero.*/
+	unsigned char start;
 
-  /* Number of characters in the range */
+	/* Number of characters in the range */
 	int count;
 
-  /* Postion of the characters in mask */
+	/* Postion of the characters in mask */
 	int pos;
 };
 
   /* Simplified mask structure for processing the mask inside a format for password generation */
 struct mask_context {
-  /* Set of mask placeholders selected for processing inside the format */
+	/* Set of mask placeholders selected for processing inside the format */
 	struct mask_range ranges[MASK_RANGES_MAX];
 
-  /* Positions in mask for overwriting in the format */
+	/* Positions in mask for overwriting in the format */
 	int activeRangePos[MASK_RANGES_MAX];
 
-  /* Number of postions for overwriting in the format */
+	/* Number of postions for overwriting in the format */
 	int count;
 
-  /* Wordlist mode flag, set to 1 when used with wordlist else set to 0 */
+	/* Wordlist mode flag, set to 1 when used with wordlist else set to 0 */
 	unsigned char flg_wrd;
 };
 
