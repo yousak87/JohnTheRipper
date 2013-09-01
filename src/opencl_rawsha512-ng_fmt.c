582a39bb1fb01914ffb1724265fad22abbd985d9
Sayantan Datta <std2048@gmail.com>
2013-09-01 10:06:32 +0530

    new format param.

--- src/opencl_rawsha512-ng_fmt.c
+++ src/opencl_rawsha512-ng_fmt.c
@@ -751,6 +751,7 @@ struct fmt_main fmt_opencl_rawsha512_ng = {
 		SALT_ALIGN_RAW,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
+		0,
 		FMT_CASE | FMT_8_BIT | FMT_SPLIT_UNIFIES_CASE,
 		raw_tests
 	}, {
